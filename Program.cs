56a5974211ec0080cbb4cf5021ba6bdad651a808
Ali-Shahmansouri-Playground <ali.shahmansouri110@gmail.com>
2023-10-05 21:10:06 +0330

    Implemet Parser Util

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Numerics;
+
+int ReadLineParsed(string message)
+{
+    Console.WriteLine(message);
+    try
+    {
+        return int.Parse(Console.ReadLine());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message + "\n Please enter a valid number.");
+        return ReadLineParsed(message);
+    }
+}
